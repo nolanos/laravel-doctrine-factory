942894cfa6b6bdcbab1a774bb4f0027cfb7425ad
Nolan Phillips <nolan@stemble.com>
2025-08-20 11:31:31 -0300

    Correct the workbench comment migration

--- workbench/database/migrations/2023_09_26_130000_create_comments.php
+++ workbench/database/migrations/2023_09_26_130000_create_comments.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('body');
-            $table->foreignId('user_id')->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->foreignId('post_id')->nullable()->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
